e08a5bf3241bf275f57cdec907445cbc6ac13fc4
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-04-24 15:06:41 +0200

    add permissions to vote on SecurityCouncilElectionGovernor

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
     arbitrumOne: {
       coreGovernor: "0xf07DeD9dC292157749B6Fd268E37DF6EA38395B9",
       treasuryGovernor: "0x789fC99093B09aD01C34DC7251D0C89ce743e5a4",
+      securityCouncilElectionGovernor:
+        "0x467923b9ae90bdb36ba88eca11604d45f13b712c",
       snapshotSigner: "0xb0382209806345d27dFdAB5Bbc17B2AB553165ac",
     },
   },
